6a39b2fbb1a922fae2a29b6c65168561c1303ea8
Jay Kim <jay.kim.2340@gmail.com>
2017-11-29 12:31:48 -0800

    created router and models

---
+++ router.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+const models = require('./models.js')
+
+router.route('/signup').post(function (req, res) {
+  console.log('got signup request', req.body);
+  models.signup.post(req.body, function (err, dataObj) {
+    if (err) {
+      console.log('err ========= ', err);
+    }
+    console.log('dataObj ========= ', dataObj);
+    res.send(dataObj);
+  })
+})
+
+router.route('/login').get(function (req, res) {
+  console.log('got login request', req.body);
+  models.login.post(req.body, function (err, dataObj) {
+    if (err) {
+      console.log('err ========= ', err);
+    }
+    console.log('dataObj ========= ', dataObj);
+    res.send(dataObj);
+  })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const router = require('./router.js')
-
+const router = require('./router.js');
+const cors = require('cors');
+const corsOptions = {
+    origin: 'http://jayop.com',
+    optionsSuccessStatus: 200
+}
 
 const port = 3000;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.resolve(__dirname, '../client')));
-
-
-app.use('/home', router);
+app.use(express.static(path.resolve(__dirname, '../Ripple_client/public')));
+app.use(cors(corsOptions));
+app.use('/main', router);
 
 app.listen(port, function(){
     console.log('listenong to port ', port)
