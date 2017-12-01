0951eaea0d582605baa41635006686bd014ca98d
Jay Kim <jay.kim.2340@gmail.com>
2017-12-01 15:01:50 -0800

    socket server created

--- socket_server/socket.js
+++ socket_server/socket.js
@@ -1,16 +1,19 @@
-import http from 'http';
-import SocketIo from 'socket.io';
-import { each } from 'lodash'; //?
-
-import clientEvents from './clientEvents';
+const express = require('express');
+const http = require('http');
+const SocketIo = require('socket.io');
 
 const server = http.createServer();
 const io = SocketIo(server);
 
-io.on('connection', (client) => {
-  log('client connected');
-  // do something on client
+io.on('connection', (socket) => {
+  socket.on('message', body => {
+    console.log('this is body =============== ', body)
+    socket.broadcast.emit('message', {
+      body: body[1],
+      from: body[0]
+    })
+  })
 });
 
 const port = process.env.PORT || 3500;
-server.listen(port, () => log(`socket server listening on port ${port}`));
+server.listen(port, () => console.log(`socket server listening on port ${port}`));
