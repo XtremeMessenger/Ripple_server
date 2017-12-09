bf11e035cf6aef64a1d3e45557dcb133f833f0b0
Jay Kim <jay.kim.2340@gmail.com>
2017-12-09 09:06:48 -0800

    working on session token

--- router.js
+++ router.js
@@ -6,56 +6,56 @@ const models = require('./models.js')
 // mongoose.Promise = bluebird;
 
 router.route('/signup').post(function (req, res) {
-  console.log('got signup request', req.body);
+  //console.log('got signup request', req.body);
   models.signup.post(req.body, function (err, dataObj) {
     if (err) {
       console.log('err ========= ', err);
     }
-    console.log('dataObj ========= ', dataObj);
+    //console.log('dataObj ========= ', dataObj);
     res.send(dataObj);
   })
 })
 
 router.route('/login').post(function (req, res) {
-  console.log('got login request', req.body);
+  //console.log('got login request', req.body);
   models.login.post(req.body, function (err, dataObj) {
     if (err) {
       console.log('err ========= ', err);
     }
-    console.log('dataObj ========= ', dataObj);
+    console.log('login dataObj to send back to client ========= ', dataObj);
     res.send(dataObj);
   })
 })
 
 router.route('/addFriend').post(function (req, res) {
-  console.log('got friend request', req.body);
+  //console.log('got friend request', req.body);
  models.addUser.post(req.body, function(err, dataObj){
   if (err) {
     console.log('err ========= ', err);
   }
   res.send(dataObj)
-  console.log('addfriend models dataObj ', dataObj);
+  //console.log('addfriend models dataObj ', dataObj);
  })
 })
 
 router.route('/privateChatStore').post(function (req, res) {
-  console.log('got friend request', req.body);
+  //console.log('got friend request', req.body);
  models.privateChatStore.post(req.body, function(err, dataObj){
   if (err) {
     console.log('err ========= ', err);
   }
-  console.log('addfriend models dataObj ', dataObj);
+  //console.log('addfriend models dataObj ', dataObj);
   res.send(dataObj);
  })
 })
 
 router.route('/getRoomChatHistory').post(function (req, res) {
-  console.log('got friend request', req.body);
+  //console.log('got friend request', req.body);
  models.getRoomChatHistory.post(req.body, function(err, dataObj){
   if (err) {
     console.log('err ========= ', err);
   }
-  console.log('addfriend models dataObj ', dataObj);
+  //console.log('addfriend models dataObj ', dataObj);
   res.send(dataObj);
  })
 })
@@ -63,7 +63,7 @@ router.route('/getRoomChatHistory').post(function (req, res) {
 
 router.route('/getRooms').post(function (req, res){
   models.getRooms.post(req.body, function(err, dataObj){
-    console.log('this is req.body ' , req.body)
+    //console.log('this is req.body ' , req.body)
     if(err) {
       console.log('err ========= ', err)
     }
@@ -120,7 +120,7 @@ router.route('/addRoom').post(function (req, res){
 // })
 
 router.route('/getPrivateChatHistory').post(function (req, res) {
-  console.log('getPrivateChatHistory ====', req.body);
+  //console.log('getPrivateChatHistory ====', req.body);
   let totalMessages = [];
 
   models.getPrivateChatHistory.post(req.body, (err, dataObj) => {
@@ -128,11 +128,11 @@ router.route('/getPrivateChatHistory').post(function (req, res) {
     if (err) {
       console.log('err ========= ', err);
     }
-    console.log('dataObj' ,dataObj)
+    // console.log('dataObj' ,dataObj)
     for (let i = 0; i < dataObj.length; i++) {
       totalMessages.push(dataObj[i].dataValues)
     }
-    console.log('srgfjwrhfwrojhgwojgheojrghrjohgojrhgojhre ', totalMessages);
+    // console.log('totalMessages ', totalMessages);
     res.send({ messages: [totalMessages] });
   })
 
@@ -140,13 +140,13 @@ router.route('/getPrivateChatHistory').post(function (req, res) {
 
 
 router.route('/getFriends').post(function (req, res) {
-  console.log('get friends request', req.body);
+  //console.log('get friends request', req.body);
  models.getFriends.post(req.body, function(err, dataObj){
   if (err) {
     console.log('err ========= ', err);
   }
   res.send(dataObj)
-  console.log('getFriends models dataObj ', dataObj);
+  //console.log('getFriends models dataObj ', dataObj);
  })
 })
 
