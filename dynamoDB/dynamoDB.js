aef8e12a4ab4d388339ba9fded9bcb4453b897d2
Jay Kim <jay.kim.2340@gmail.com>
2017-12-12 13:54:12 -0800

    working on dynamoDB

---
+++ dynamoDB/dynamoDB.js
@@ -0,0 +1,50 @@
+const AWS = require("aws-sdk");
+AWS.config.loadFromPath('./config/AWSKey.json');
+
+var dynamodb = new AWS.DynamoDB();
+
+dynamodb.createTable({
+  TableName: "Messages",
+  KeySchema: [
+    { AttributeName: "from", KeyType: "HASH" },  //Partition key
+    { AttributeName: "to", KeyType: "RANGE" }  //Sort key
+  ],
+  AttributeDefinitions: [
+    { AttributeName: "from", AttributeType: "N" },
+    { AttributeName: "to", AttributeType: "S" }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 10,
+    WriteCapacityUnits: 10
+  }
+}, function (err, data) {
+  if (err) {
+    console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+  } else {
+    console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+  }
+});
+
+
+var Movies = {
+  TableName: "Movies",
+  KeySchema: [
+    { AttributeName: "year", KeyType: "HASH" },  //Partition key
+    { AttributeName: "title", KeyType: "RANGE" }  //Sort key
+  ],
+  AttributeDefinitions: [
+    { AttributeName: "year", AttributeType: "N" },
+    { AttributeName: "title", AttributeType: "S" }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 10,
+    WriteCapacityUnits: 10
+  }
+};
+dynamodb.createTable(Movies, function (err, data) {
+  if (err) {
+    console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+  } else {
+    console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+  }
+});
